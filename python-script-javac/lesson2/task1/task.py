92b9b0ec3f0b6141b6798bf07e8a1f0dabad3866
John Meyer <0x326@users.noreply.github.com>
2017-10-24 15:42:35 -0400

    Fix variable name mismatch

--- python-script-javac/lesson2/task1/task.py
+++ python-script-javac/lesson2/task1/task.py
@@ -1,8 +1,8 @@
 from fs import open_fs
 
-documents_folder = open_fs('~/Downloads/')
-with documents_folder:
-    for filename in documents_folder.listdir('/'):  # type: str
+downloads_folder = open_fs('~/Downloads/')
+with downloads_folder:
+    for filename in downloads_folder.listdir('/'):  # type: str
         print(filename)
 
     # documents_folder is automatically closed
